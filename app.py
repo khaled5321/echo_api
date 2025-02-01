22a5ec6076f4ef82979dc46e5f31b0fb6efa4a32
Khaled Emad <khaledemad656@gmail.com>
2025-02-01 13:44:03 +0200

    ECHO API

---
+++ app.py
@@ -0,0 +1,23 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.post("/echo")
+async def echo(data):
+    return {"received": f"{data}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
